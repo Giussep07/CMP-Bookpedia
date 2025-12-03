a223e481b935e60326ee506bb6f0458f44a5520d
giussepr <giussepr@gmail.com>
2025-12-03 07:50:25 -0500

    Search for books from Book list viewmodel

--- composeApp/src/commonMain/kotlin/dev/giussepr/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/dev/giussepr/bookpedia/App.kt
@@ -3,16 +3,30 @@ package dev.giussepr.bookpedia
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import dev.giussepr.bookpedia.book.data.network.KtorRemoteBookDataSource
+import dev.giussepr.bookpedia.book.data.repository.DefaultBookRepository
 import dev.giussepr.bookpedia.book.presentation.book_list.BookListRoot
 import dev.giussepr.bookpedia.book.presentation.book_list.BookListViewModel
+import dev.giussepr.bookpedia.core.data.HttpClientFactory
+import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(engine: HttpClientEngine) {
     MaterialTheme {
         BookListRoot(
-            viewModel = remember { BookListViewModel() },
+            viewModel = remember {
+                BookListViewModel(
+                    repository = DefaultBookRepository(
+                        remoteBookDataSource = KtorRemoteBookDataSource(
+                            httpClient = HttpClientFactory.create(
+                                engine = engine
+                            )
+                        )
+                    )
+                )
+            },
             onBookClick = {}
         )
     }
--- composeApp/src/commonMain/kotlin/dev/giussepr/bookpedia/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/giussepr/bookpedia/book/presentation/book_list/BookListViewModel.kt
@@ -1,16 +1,46 @@
+@file:OptIn(FlowPreview::class)
+
 package dev.giussepr.bookpedia.book.presentation.book_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.giussepr.bookpedia.book.domain.Book
 import dev.giussepr.bookpedia.book.domain.BookRepository
+import dev.giussepr.bookpedia.core.domain.onError
+import dev.giussepr.bookpedia.core.domain.onSuccess
+import dev.giussepr.bookpedia.core.presentation.toUiText
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class BookListViewModel(
     private val repository: BookRepository
 ) : ViewModel() {
+    private var cachedBooks: List<Book> = emptyList()
+    private var searchJob: Job? = null
+
     private val _state = MutableStateFlow(BookListState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if (cachedBooks.isEmpty()) {
+                observeSearchQuery()
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            _state.value
+        )
 
     fun onIntent(intent: BookListIntent) {
         when (intent) {
@@ -33,4 +63,56 @@ class BookListViewModel(
         }
     }
 
+    private fun observeSearchQuery() {
+        state
+            .map { it.searchQuery }
+            .distinctUntilChanged()
+            .debounce(500L)
+            .onEach { query ->
+                when {
+                    query.isBlank() -> {
+                        _state.update {
+                            it.copy(
+                                errorMessage = null,
+                                searchResults = cachedBooks
+                            )
+                        }
+                    }
+
+                    query.length >= 2 -> {
+                        searchJob?.cancel()
+                        searchJob = searchBooks(query)
+                    }
+                }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun searchBooks(query: String) =
+        viewModelScope.launch {
+            _state.update {
+                it.copy(isLoading = true)
+            }
+
+            repository
+                .searchBooks(query)
+                .onSuccess { searchResults ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = null,
+                            searchResults = searchResults
+                        )
+                    }
+                }
+                .onError { error ->
+                    _state.update {
+                        it.copy(
+                            searchResults = emptyList(),
+                            isLoading = false,
+                            errorMessage = error.toUiText()
+                        )
+                    }
+                }
+        }
 }
